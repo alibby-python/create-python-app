fa5136b6ed360cbec4691964341821d33f2378d7
Alex Libby <cpa@cutmango.com>
2026-09-19 15:10:49 +0100

    docs: add missing character in config

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -6,8 +6,8 @@ export default defineConfig({
 	site: 'https://alibby-python.github.io',
 
 	// Remove when custom domain is enabled.
-	base: 'create-react-app',
-	
+	base: '/create-react-app',
+
 	integrations: [
 		starlight({
 			title: "Create Python App",
